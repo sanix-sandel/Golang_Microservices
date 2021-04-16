c892c55aefc3019203b2c523618543a91997f065
sanix <sanicksikani@gmail.com>
2021-04-16 19:00:05 +0300

    Server with shutdown

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/Go_Microservices/handlers"
 )
@@ -17,5 +20,31 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gb)
 
-	http.ListenAndServe(":8000", sm)
+	//Setup server
+	s := &http.Server{
+		Addr:         ":8000",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	//	s.ListenAndServe()
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second) //timeout context
+	s.Shutdown(tc)
 }
